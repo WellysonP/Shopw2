9fc3048d82d73de758feef8a0b5260af73a18852
Wellyson Pontes <wellysonpontes@gmail.com>
2022-05-13 14:22:52 -0300

    Definido tela de carrinho e configurado para mostrar preço total dos itens acumulados totalAcount

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop2/models/cart_item.dart';
 import '../models/cart.dart';
 
@@ -7,9 +8,49 @@ class CartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Cart cart = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Carrinho de compras"),
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: EdgeInsets.all(20),
+            elevation: 10,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                // mainAxisAlignment: MainAxisAlignment.center
+                children: [
+                  Text(
+                    "Total:",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Chip(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    label: Text(
+                      "R\$ ${cart.totalAmount}",
+                      style: TextStyle(
+                        color:
+                            Theme.of(context).primaryTextTheme.headline6?.color,
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text("COMPRAR"),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
