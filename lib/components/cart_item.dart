0a609d51e5297e70b640c817dd8a64c8b0b8833a
Wellyson Pontes <wellysonpontes@gmail.com>
2022-05-16 13:46:19 -0300

    incluido Dismissible no carrinho

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop2/models/cart.dart';
 
 import '../models/cart_item.dart';
 
@@ -8,6 +10,54 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(cartItem.name);
+    return Dismissible(
+      key: ValueKey(cartItem.id),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        margin: EdgeInsets.symmetric(
+          vertical: 4,
+          horizontal: 15,
+        ),
+      ),
+      onDismissed: (_) {
+        Provider.of<Cart>(
+          context,
+          listen: false,
+        ).removeItem(cartItem.productId);
+      },
+      child: Card(
+        elevation: 5,
+        margin: const EdgeInsets.symmetric(
+          vertical: 4,
+          horizontal: 15,
+        ),
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            child: Padding(
+              padding: const EdgeInsets.all(2),
+              child: FittedBox(
+                child: Text(
+                  "${cartItem.price.toStringAsFixed(2)}",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+          title: Text(cartItem.name),
+          subtitle: Text(
+              "Total: R\$ ${(cartItem.price * cartItem.quantity).toStringAsFixed(2)}"),
+          trailing: Text("${cartItem.quantity} x"),
+        ),
+      ),
+    );
   }
 }
